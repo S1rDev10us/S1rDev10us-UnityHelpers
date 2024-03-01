983df41514a7b890db45019bebd30e2991638c6a
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-03-01 11:55:45 +0000

    Added colour extensions

---
+++ ExtensionMethods/ColourExtensions.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class ColourExtensions {
+	public static Color With(this Color colour, float? r = null, float? g = null, float? b = null, float? a = null)	{
+		return new Color(r ?? colour.r, g ?? colour.g, b ?? colour.b, a ?? colour.a);
+	}
+}
